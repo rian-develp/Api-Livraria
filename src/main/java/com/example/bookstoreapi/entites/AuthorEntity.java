a88c17f5c417a7c640c3011d5f88eb3c00be3973
rian-develp <riandeveloper1@gmail.com>
2025-05-02 17:04:01 -0300

    Implementando atributos e métodos da entidade author

--- src/main/java/com/example/bookstoreapi/entites/AuthorEntity.java
+++ src/main/java/com/example/bookstoreapi/entites/AuthorEntity.java
@@ -1,4 +1,39 @@
 package com.example.bookstoreapi.entites;
 
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "authors")
 public class AuthorEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long code;
+    @Column(length = 30)
+    private String citizen;
+    @Column(nullable = false, length = 46)
+    private String name;
+
+    public Long getCode() {
+        return code;
+    }
+
+    public void setCode(Long code) {
+        this.code = code;
+    }
+
+    public String getCitizen() {
+        return citizen;
+    }
+
+    public void setCitizen(String citizen) {
+        this.citizen = citizen;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
