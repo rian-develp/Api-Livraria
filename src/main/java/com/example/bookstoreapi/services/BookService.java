777b87b29249c88e528be2cee358c4a646f009fd
rian-develp <riandeveloper1@gmail.com>
2025-05-15 11:38:59 -0300

    Criando o service da entidade book

---
+++ src/main/java/com/example/bookstoreapi/services/BookService.java
@@ -0,0 +1,41 @@
+package com.example.bookstoreapi.services;
+
+import com.example.bookstoreapi.entites.BookEntity;
+import com.example.bookstoreapi.repositories.BookRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.sql.Date;
+import java.util.List;
+import java.util.Optional;
+
+public class BookService {
+
+    @Autowired
+    private BookRepository repository;
+
+    public List<BookEntity> getAllBooks(){
+        return repository.findAll();
+    }
+
+    public Optional<BookEntity> getBookByCode(Long code){
+        return repository.findById(code);
+    }
+
+    public boolean insertBook(
+        String authorName, Double price,
+        Date publishDate, Integer quantity,
+        String title
+    ){
+        if( authorName == null || authorName.isBlank() ||
+            title == null || title.isBlank() ||
+            price == null || publishDate == null ||
+            quantity == null
+        ){
+            return false;
+        }
+
+        BookEntity entity = new BookEntity(authorName, price, publishDate, quantity, title);
+        repository.save(entity);
+        return true;
+    }
+}
