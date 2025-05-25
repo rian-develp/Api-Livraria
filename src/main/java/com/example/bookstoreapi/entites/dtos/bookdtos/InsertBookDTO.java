86ff65f24ca6beaffa52cd773f361787a339dbd9
rian-develp <riandeveloper1@gmail.com>
2025-05-25 00:37:08 -0300

    Dtos refatorados

--- src/main/java/com/example/bookstoreapi/entites/dtos/bookdtos/InsertBookDTO.java
+++ src/main/java/com/example/bookstoreapi/entites/dtos/bookdtos/InsertBookDTO.java
@@ -1,13 +1,25 @@
 package com.example.bookstoreapi.entites.dtos.bookdtos;
 
 
-import java.sql.Date;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import java.time.LocalDate;
 
 public record InsertBookDTO(
+    @NotNull(message = "Nome do autor não pode ser null")
+    @NotBlank(message = "Nome do autor deve ser preenchido")
     String authorName,
+    @NotNull(message = "Preço não pode ser null")
+    @NotBlank(message = "Preço deve ser preenchido")
     Double price,
-    Date publishDate,
+    @NotNull(message = "Data não pode ser null")
+    @NotBlank(message = "Data deve ser preenchida")
+    LocalDate publishDate,
+    @NotNull(message = "Preço não pode ser null")
+    @NotBlank(message = "Preço deve ser preenchido")
     Integer quantity,
+    @NotNull(message = "Título não pode ser null")
+    @NotBlank(message = "Título deve ser preenchido")
     String title
 ){
 }
