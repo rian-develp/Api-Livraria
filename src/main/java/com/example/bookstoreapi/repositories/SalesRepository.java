86b157684900c89f9da34b70af963f8efa488752
rian-develp <riandeveloper1@gmail.com>
2025-09-29 23:49:03 -0300

    Refatorando o repositório SalesRepository.java

--- src/main/java/com/example/bookstoreapi/repositories/SalesRepository.java
+++ src/main/java/com/example/bookstoreapi/repositories/SalesRepository.java
@@ -2,6 +2,24 @@ package com.example.bookstoreapi.repositories;
 
 import com.example.bookstoreapi.entites.SalesEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface SalesRepository extends JpaRepository<SalesEntity, Long> {
+
+    @Query("SELECT s FROM SalesEntity s " +
+            "JOIN FETCH s.book " +
+            "JOIN FETCH s.customer"
+    )
+    List<SalesEntity> getAllSalesDetails();
+
+    @Query("SELECT s FROM SalesEntity s " +
+            "JOIN FETCH s.book b " +
+            "JOIN FETCH s.customer c " +
+            "WHERE c.id =: customerId"
+    )
+    List<SalesEntity> getAllSalesDetailsByCustomerId(@Param("customerId") Long customerId);
+
 }
